fbe2e000cdb020e851c2f7d17c8d65432967a41f
gabriela martinez <gabrielaale@gmail.com>
2013-06-20 22:14:37 -0430

    Cambio para el screenshot

--- TablaApp/TablaApp/ViewController.m
+++ TablaApp/TablaApp/ViewController.m
@@ -18,7 +18,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    tableData = [NSArray arrayWithObjects:@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini", nil];
+    tableData = [NSArray arrayWithObjects:@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", nil];
 }
 
 - (void)didReceiveMemoryWarning
